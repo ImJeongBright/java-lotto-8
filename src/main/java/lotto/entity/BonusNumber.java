2ee485b017564d1da415b7354941c1291a369a07
notYourOcean <wjswjdgus333@gmail.com>
2025-11-03 00:49:30 +0900

    feat : BonusNumber 추가

--- src/main/java/lotto/entity/BonusNumber.java
+++ src/main/java/lotto/entity/BonusNumber.java
@@ -1,6 +1,19 @@
 package lotto.entity;
 
-public class BonusNumber {
+import java.util.List;
 
+public class BonusNumber {
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public boolean isBonusNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if(number == bonusNumber) return true;
+        }
+        return false;
+    }
 
 }
